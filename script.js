4eb2ce27013063e73998f37070df9509e281926b
lidianealmeida <lidiane_almeidaa@hotmail.com>
2019-03-27 10:10:37 -0300

    vs 0.0.1 - link de cadastro da pagina login funcionando, adicionado pagina perfil e css

--- script.js
+++ script.js
@@ -7,21 +7,37 @@ $(document).ready(function () {
     let password = $("#exampleInputPassword1").val();
 
     firebase.auth().createUserWithEmailAndPassword(email, password)
-    
-    .then(function(response){
-      
-      console.log(response)
-    })
-    
-      .catch(function(error) {
-      // Handle Errors here.
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      alert(errorMessage)
-      // ...
-    });
+      .then(function (response) {
+        window.location = "page.html";
+        console.log(response)
+      })
 
+      .catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        alert(errorMessage)
+        // ...
+      });
   })
+
+  // Botão cadastrar
+  $("#btn btn-primary").click(function (event) {
+    event.preventDefault();
+    let email = $("#exampleInputEmail1").val();
+    let password = $("#exampleInputPassword1").val();
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(function () {
+        window.location = "page.html";
+      })
+
+      .catch(function (error) {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        alert(errorMessage)
+      });
+
+  });
+
 })
-
-
