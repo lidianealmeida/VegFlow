bf4e9c587fee5e8a313dcaf761ba21fb07646aa9
lidianealmeida <lidiane_almeidaa@hotmail.com>
2019-04-10 14:28:13 -0300

    feito readme.md, textarea não redimensionando, feito botão like com contador

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
 var database = firebase.database();
-var USER_ID = window.location.search.match(/\?id=(.*)/);
+var USER_ID = window.location.search.match(/\?id=(.*)/)[1];
 
 $(document).ready(function () {
-
   getPostFromDB();
   $(".btn-post").click(addPostClick);
   });
@@ -12,15 +11,16 @@ function addPostClick (event){
     event.preventDefault();
     let newPost = $(".input-post").val();
     let postFromDB = addPostDB(newPost);
+    let likeInitial = 0;
       
-    createListPost(newPost, postFromDB.key)
+    createListPost(newPost, postFromDB.key, likeInitial)
 }
 
 // Adicionar novo post ao database
 function addPostDB(newPost){
   return database.ref("post/" + USER_ID).push({
-    // name: ???????
-    text: newPost
+    text: newPost,
+    like: 0
   });  
 }
 
@@ -31,56 +31,73 @@ function getPostFromDB (){
     snapshot.forEach(function (childSnapshot) {
       const childKey = childSnapshot.key;
       const childData = childSnapshot.val();
-       createListPost(childData.text, childKey ) 
+       createListPost(childData.text, childKey, childData.like) 
        });
       });
 }
 
 // Adiciona lista de post e inclui botão editar e excluir
-function createListPost(newPost, key){
-  $(".input-post").val("");
-  $(".post-list").append(`
+  function createListPost(newPost, key, likeInitial){
 
-  <div class="post-feed-box-public" data-post-box-public-id=${key}>
-  <div data-image-id=${key}>
-    <img class="post-feed-image" width="60px" height="60px" src="images/persona.jpg" />
-  </div>
-
+    $(".input-post").val("");
+    $(".post-list").append(`
   
-  <p>
-  <span data-newPost-id="${key}" >${newPost} </span><br>
-  <input type="button" class=" btn-light" value="Editar" data-edit-id=${key} />
-  <input type="button" class=" btn-light" value="Excluir" data-delete-id=${key} />
-  <p>`);
-
-    $(`input[data-delete-id="${key}"]`).click(function () {
-      database.ref("/post/" + USER_ID + "/" + key).remove();
-      $(this).parent().remove();
-      $(`div[data-post-box-public-id="${key}"]`).remove();
+    <div class="post-feed-box-public" data-post-box-public-id=${key}>
+    <div data-image-id=${key}>
+      <img class="post-feed-image" width="60px" height="60px" src="images/persona.jpg" />
+    </div>
+  
+    
+    <p>
+    <span data-newPost-id="${key}" >${newPost} </span><br>
+    <input type="button" value="Editar" data-edit-id=${key} />
+    <input type="button" value="Excluir" data-delete-id=${key} />
+   
+    <i data-toggle="modal" class="favorite-modal" data-id=${key} data-target="#favorite-post-modal">Like</i>
+    <i data-toggle="modal" class="favorite-count-modal" data-id=${key} data-target="#favorite-count-modal"> ${likeInitial}</i>
+  
+    <p>`);
+  
+      $(`input[data-delete-id="${key}"]`).click(function () {
+        let confirmDel = confirm("Confirma a exclusão da postagem?")
+        if(confirmDel){
+        database.ref("/post/" + USER_ID + "/" + key).remove();
+        $(this).parent().remove();
+        $(`div[data-post-box-public-id="${key}"]`).remove();
+        }
+      });
+  
+      $(`input[data-edit-id="${key}"]`).click(function () {
+      const newText = prompt(`Altere o seu post: ${newPost}`);
+      $(`span[data-newPost-id=${key}]`).text(newText);  
+      database.ref("/post/" + USER_ID + "/" + key).update({
+        text: newText
+      })
+     });
+  
+     $(`.favorite-modal[data-id=${key}]`).click(function fav(){
+      let favNum = $(`.favorite-count-modal[data-id=${key}]`).html();
+      favNum = parseInt(favNum)+1;
+      $(`.favorite-count-modal[data-id=${key}]` ).html(favNum);
+      database.ref("/post/" + USER_ID + "/" + key ).update({like: favNum });
     });
-
-    $(`input[data-edit-id="${key}"]`).click(function () {
-    const newText = prompt(`Altere o seu post: ${newPost}`);
-    $(`span[data-newPost-id=${key}]`).text(newText);  
-    database.ref("/post/" + USER_ID + "/" + key).update({
-      text: newText
-    })
-   });
+   
   }
 
-    // Confirmação de excluir
-  // document.getElementsByClassName
-  //   function myFunction() {
-  //     var txt;
-  //     var r = confirm("Press a button!");
-  //     if (r == true) {
-  //       txt = "You pressed OK!";
-  //     } else {
-  //       txt = "You pressed Cancel!";
-  //     }
-  //     document.getElementById("demo").innerHTML = txt;
-  //   }
-  
+// Botão like
+function likePost() {
+  let heartIcon = $(".fa-heart");
+  heartIcon.click(function () {
+    let curtidas = $(this)
+
+    curtidas.val(Number(curtidas.val()) + 1);
+    $(".fa-heart").removeClass('far').addClass('fas');
+    console.log(curtidas.val())
+    $('#likes').html(curtidas.val())
+  })
+
+}
+likePost()
 
 // Configurações da textarea e contador de caracteres 
 const charCounter = $(".character-counter");
@@ -92,35 +109,30 @@ newPost.keyup(startCounting);
 
 // Habilitar e desabilitar botão publicar
 function enableButton() {
-  addNewPost.prop("disabled", false);
-  addNewPost.css("cursor", "pointer");
-  addNewPost.css("opacity", "1");
+addNewPost.prop("disabled", false);
+addNewPost.css("cursor", "pointer");
+addNewPost.css("opacity", "1");
 }
 
 function disableButton() {
-  addNewPost.css("cursor", "not-allowed");
-  addNewPost.prop("disabled", true);  
+addNewPost.css("cursor", "not-allowed");
+addNewPost.prop("disabled", true);  
 }
 
 // Alteração cor contador e ajuste dimensões textarea
 function startCounting() {
-  const charAllowed = 140;
-  const typedChar = newPost.val().length;
-  let remainingChar = charAllowed - typedChar;
-  charCounter.text(remainingChar);
-  if (typedChar >= 130 && typedChar <= 140) {
-    charCounter.css("color", "orange");
-  } else if (typedChar > 140 || typedChar === 0 || !newPost.val().trim()) { 
-    disableButton();
-    if (typedChar > 140) {
-      charCounter.css("color", "red");
-    }
-  } else {
-    charCounter.css("color", "black");  
+const charAllowed = 140;
+const typedChar = newPost.val().length;
+let remainingChar = charAllowed - typedChar;
+charCounter.text(remainingChar);
+if (typedChar >= 130 && typedChar <= 140) {
+  charCounter.css("color", "orange");
+} else if (typedChar > 140 || typedChar === 0 || !newPost.val().trim()) { 
+  disableButton();
+  if (typedChar > 140) {
+    charCounter.css("color", "red");
   }
-  const lines = newPost.val().split("\n");
-  const linesSize = lines.map(x => Math.max(1, Math.ceil(x.length / 38)));
-  newPost.attr("rows", linesSize.reduce((acum, index) => acum + index));
+} else {
+  charCounter.css("color", "black");  
+  }
 }
-
-
