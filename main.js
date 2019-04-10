8b06e8c13a870495160e4098f3a9e0d2491054f5
JayFerreira <jayzoca@gmail.com>
2019-04-10 19:03:01 -0300

    inclusao de carrossel na página inicial

--- main.js
+++ main.js
@@ -13,46 +13,47 @@ $(document).ready(function () {
     $(".profile-name").text(userInfo.name);
     $(".profile-user").text(userInfo.email); 
     $(".profile-diet-type").text(userInfo.diet);
+});
+
+$(".btn-all-posts").click(function(){
+  database.ref("/post/" + USER_ID).once('value')
+    .then(function (snapshot) {
+      $(".post-list").html("");
+      snapshot.forEach(function (childSnapshot) {
+        const childKey = childSnapshot.key;
+        const childData = childSnapshot.val();
+     createListPost(childData.text, childKey, childData.like ) 
+    })
   });
+});
 
-  $(".btn-all-posts").click(function(){
-    database.ref("/post/" + USER_ID).once('value')
-      .then(function (snapshot) {
-        $(".post-list").html("");
-        snapshot.forEach(function (childSnapshot) {
-          const childKey = childSnapshot.key;
-          const childData = childSnapshot.val();
-       createListPost(childData.text, childKey, childData.like ) 
-      })
+$(".btn-filter-private").click(function(){ 
+  database.ref("/post/" + USER_ID).orderByChild("type").equalTo("privado")
+    .once('value', function (snapshot){
+      $(".post-list").html("");
+      snapshot.forEach(function (childSnapshot) {
+        const childKey = childSnapshot.key;
+        const childData = childSnapshot.val();
+      createListPost(childData.text, childKey, childData.like )
     });
   });
-
-  $(".btn-filter-private").click(function(){ 
-    database.ref("/post/" + USER_ID).orderByChild("type").equalTo("privado")
-      .once('value', function (snapshot){
-        $(".post-list").html("");
-        snapshot.forEach(function (childSnapshot) {
-          const childKey = childSnapshot.key;
-          const childData = childSnapshot.val();
-        createListPost(childData.text, childKey, childData.like )
-      });
-    });
-  })
-
-  $(".btn-filter-public").click(function(){ 
-    database.ref("/post/" + USER_ID).orderByChild("type").equalTo("publico")
-      .once('value', function (snapshot){
-        $(".post-list").html("");
-        snapshot.forEach(function (childSnapshot) {
-          const childKey = childSnapshot.key;
-          const childData = childSnapshot.val();
-        createListPost(childData.text, childKey, childData.like )
-      });
-    });
-  })
 })
-  // Inicia a função pegando os valores ao click do botão postar
-  function addPostClick (event){
+
+$(".btn-filter-public").click(function(){ 
+  database.ref("/post/" + USER_ID).orderByChild("type").equalTo("publico")
+    .once('value', function (snapshot){
+      $(".post-list").html("");
+      snapshot.forEach(function (childSnapshot) {
+        const childKey = childSnapshot.key;
+        const childData = childSnapshot.val();
+      createListPost(childData.text, childKey, childData.like )
+    });
+  });
+})
+})
+
+// Inicia a função pegando os valores ao click do botão postar
+function addPostClick (event){
     event.preventDefault();
     var newPost = $(".input-post").val();
     var optionPrivate = $("#private-options option:selected").val();
@@ -60,19 +61,19 @@ $(document).ready(function () {
     var likeInitial = 0;
 
     createListPost(newPost, postFromDB.key,likeInitial)
-  }
+}
 
-  // Adicionar novo post ao database
-  function addPostDB(newPost,optionPrivate){
-    return database.ref("post/" + USER_ID).push({
-     text: newPost,
-     like: 0,
-     type: optionPrivate
-    });  
-  }
- 
-  // Adicionar novo user ao database
- function getPostFromDB (){
+// Adicionar novo post ao database
+function addPostDB(newPost,optionPrivate){
+  return database.ref("post/" + USER_ID).push({
+    text: newPost,
+    like: 0,
+    type: optionPrivate
+  });  
+}
+
+// Adicionar novo user ao database
+function getPostFromDB (){
   database.ref("/post/" + USER_ID).once('value')
   .then(function (snapshot) {
     snapshot.forEach(function (childSnapshot) {
@@ -83,10 +84,8 @@ $(document).ready(function () {
        });
       });
 }
-
-
 // Adiciona lista de post e inclui botão editar e excluir
-function createListPost(newPost, key,likeInitial){
+  function createListPost(newPost, key,likeInitial){
 
     $(".input-post").val("");
     $(".post-list").append(`
@@ -116,33 +115,39 @@ function createListPost(newPost, key,likeInitial){
         $(this).parent().remove();
         $(`div[data-post-box-public-id="${key}"]`).remove();
         }
-      }); 
+      });
   
-
-   $(`.favorite-modal[data-id=${key}]`).click(function fav(){
-    let favNum = $(`.favorite-count-modal[data-id=${key}]`).html();
-    favNum = parseInt(favNum)+1;
-    $(`.favorite-count-modal[data-id=${key}]` ).html(favNum);
-    database.ref("/post/" + USER_ID + "/" + key ).update({like: favNum });
-  });
+      $(`input[data-edit-id="${key}"]`).click(function () {
+      const newText = prompt(`Altere o seu post: ${newPost}`);
+      $(`span[data-newPost-id=${key}]`).text(newText);  
+      database.ref("/post/" + USER_ID + "/" + key).update({
+        text: newText
+      })
+     });
+  
+     $(`.favorite-modal[data-id=${key}]`).click(function fav(){
+      let favNum = $(`.favorite-count-modal[data-id=${key}]`).html();
+      favNum = parseInt(favNum)+1;
+      $(`.favorite-count-modal[data-id=${key}]` ).html(favNum);
+      database.ref("/post/" + USER_ID + "/" + key ).update({like: favNum });
+    });
    
   }
 
-  // Botão like
-  function likePost() {
-    let heartIcon = $(".fa-heart");
-    heartIcon.click(function () {
-      let curtidas = $(this)
+// Botão like
+function likePost() {
+  let heartIcon = $(".fa-heart");
+  heartIcon.click(function () {
+    let curtidas = $(this)
 
-  
-      curtidas.val(Number(curtidas.val()) + 1);
-      $(".fa-heart").removeClass('far').addClass('fas');
-      console.log(curtidas.val())
-      $('#likes').html(curtidas.val())
-    })
-  
-  }
-  likePost()
+    curtidas.val(Number(curtidas.val()) + 1);
+    $(".fa-heart").removeClass('far').addClass('fas');
+    console.log(curtidas.val())
+    $('#likes').html(curtidas.val())
+  })
+
+}
+likePost()
 
 // Configurações da textarea e contador de caracteres 
 const charCounter = $(".character-counter");
@@ -153,33 +158,31 @@ newPost.keydown(startCounting, enableButton);
 newPost.keyup(startCounting);
 
 // Habilitar e desabilitar botão publicar
-
-
 function enableButton() {
-  addNewPost.prop("disabled", false);
-  addNewPost.css("cursor", "pointer");
-  addNewPost.css("opacity", "1");
+addNewPost.prop("disabled", false);
+addNewPost.css("cursor", "pointer");
+addNewPost.css("opacity", "1");
 }
 
 function disableButton() {
-  addNewPost.css("cursor", "not-allowed");
-  addNewPost.prop("disabled", true);  
+addNewPost.css("cursor", "not-allowed");
+addNewPost.prop("disabled", true);  
 }
 
 // Alteração cor contador e ajuste dimensões textarea
 function startCounting() {
-  const charAllowed = 140;
-  const typedChar = newPost.val().length;
-  let remainingChar = charAllowed - typedChar;
-  charCounter.text(remainingChar);
-  if (typedChar >= 130 && typedChar <= 140) {
-    charCounter.css("color", "orange");
-  } else if (typedChar > 140 || typedChar === 0 || !newPost.val().trim()) { 
-    disableButton();
-    if (typedChar > 140) {
-      charCounter.css("color", "red");
-    }
-  } else {
-    charCounter.css("color", "black");  
+const charAllowed = 140;
+const typedChar = newPost.val().length;
+let remainingChar = charAllowed - typedChar;
+charCounter.text(remainingChar);
+if (typedChar >= 130 && typedChar <= 140) {
+  charCounter.css("color", "orange");
+} else if (typedChar > 140 || typedChar === 0 || !newPost.val().trim()) { 
+  disableButton();
+  if (typedChar > 140) {
+    charCounter.css("color", "red");
+  }
+} else {
+  charCounter.css("color", "black");  
   }
 }
