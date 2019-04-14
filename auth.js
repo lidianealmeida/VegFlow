16c29b4394244957238c55bd32448e179581ff5b
Rafael Cerri <rafaelbcerri@gmail.com>
2019-04-14 10:15:58 -0300

    Refactor

--- auth.js
+++ auth.js
@@ -1,90 +1,67 @@
-var database = firebase.database(); 
-
 $(document).ready(function () {
-
-  $("#btn-cadastrar").click(function (event) {
+  $("#btn-cadastrar").click(event => {
     event.preventDefault();
-    var name = $(".sign-name").val(); 
-    var email = $(".sign-email").val();
-    var password = $(".sign-password").val(); 
-    var diet = $(".sign-diet").val(); 
+    const name = $(".sign-name").val(); 
+    const email = $(".sign-email").val();
+    const password = $(".sign-password").val(); 
+    const diet = $(".sign-diet").val(); 
 
     firebase.auth().createUserWithEmailAndPassword(email, password)
-    .then(function(response){
-    var userId = response.user.uid;
-    writeUserData(userId, name, email,diet);
-    redirect(userId);
-  })
-   .catch(function (error) {
-    errorMessage(error); 
-    });
-  })
-
-  $(".btn-login").click(function (event) {
-    event.preventDefault();
-    var email = $(".sign-email-login").val();
-    var password = $(".sign-password-login").val();
-
-    firebase.auth().signInWithEmailAndPassword(email, password)
-      .then(function (response) {
-        var userId = response.user.uid;
+      .then((response) => {
+        const userId = response.user.uid;
+        writeUserData(userId, name, email,diet);
         redirect(userId);
       })
-
-      .catch(function (error) {
-        errorMessage(error); 
-      });
-  })
-
-  $(".sign-google").click(function (event) {
-    event.preventDefault();
-    console.log("aqui");
-    var provider = new firebase.auth.GoogleAuthProvider();
-
-    firebase.auth().signInWithPopup(provider).then(function (result) {
-      var token = result.credential.accessToken;
-      var userId = result.user;
-      redirect(userId);
-    })
-
-      .catch(function (error) {
-
-        errorMessage(error); 
-      });
+      .catch((error) => errorMessage(error));
   });
 
-  $(".btn-signout").click(function (event) {
+  $(".btn-login").click(event => {
+    event.preventDefault();
+    const email = $(".sign-email-login").val();
+    const password = $(".sign-password-login").val();
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(response => {
+        const userId = response.user.uid;
+        redirect(userId);
+      })
+      .catch(error => errorMessage(error));
+  });
+
+  $(".sign-google").click(event => {
+    event.preventDefault();
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    firebase.auth().signInWithPopup(provider)
+      .then(result => {
+        const userId = result.user;
+        redirect(userId);
+      })
+      .catch(error => errorMessage(error));
+  });
+
+  $(".btn-signout").click(event => {
     event.preventDefault();
     firebase.auth().signOut()
-      .then(function () {
-        window.location = "index.html";
-      })
-      .catch(function (error) {
-        errorMessage(error); 
-
-      });
+      .then(() => window.location = "index.html")
+      .catch(error => errorMessage(error));
   });
 });
 
-  function writeUserData(userId, name, email, diet) {
-    database.ref('users/' + userId).set({
-      name: name,
-      email: email,
-      diet: diet
-    });
-  }
+function writeUserData(userId, name, email, diet) {
+  firebase.database().ref('users/' + userId).set({
+    name: name,
+    email: email,
+    diet: diet
+  });
+}
 
-  function redirect(userId){
-    window.location = "main.html?id=" + userId;
-  }
+function redirect(userId){
+  window.location = "main.html?id=" + userId;
+}
 
-  function errorMessage(error){
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    alert(errorCode,errorMessage);
-  }
-
-
-
-
-
+function errorMessage(error){
+  const errorCode = error.code;
+  const errorMessage = error.message;
+  alert(errorCode,errorMessage);
+}
